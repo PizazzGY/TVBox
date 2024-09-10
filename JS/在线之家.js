ce6c3d6f6f1bf5b32dfd9690382e88e1c1d82231
潇洒 <78096245+PizazzGY@users.noreply.github.com>
2024-09-10 08:56:41 +0800

    Add files via upload

--- JS/在线之家.js
+++ JS/在线之家.js
@@ -1,31 +1,22 @@
-// 在线之家地址发布页 https://www.zxzj.site
-
 muban.首图2.二级.desc = '.data:eq(3)&&Text;;;.data:eq(1)&&Text;.data:eq(2)&&Text';
 var rule = {
-	title:'在线之家',
-	模板:'首图2',
-	host:'https://www.zxzj.site',
-	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(0)&&a:eq(2)&&href");print(src);HOST=src',
-	url:'/vodshow/fyclassfyfilter.html',
-	filterable:1,//是否启用分类筛选,
-	filter_url:'-{{fl.area}}-{{fl.by}}-{{fl.class}}-----fypage---{{fl.year}}',
-	filter: {
-		"1":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"喜剧","v":"喜剧"},{"n":"爱情","v":"爱情"},{"n":"恐怖","v":"恐怖"},{"n":"动作","v":"动作"},{"n":"科幻","v":"科幻"},{"n":"剧情","v":"剧情"},{"n":"战争","v":"战争"},{"n":"警匪","v":"警匪"},{"n":"犯罪","v":"犯罪"},{"n":"动画","v":"动画"},{"n":"奇幻","v":"奇幻"},{"n":"冒险","v":"冒险"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"香港","v":"香港"},{"n":"台湾","v":"台湾"},{"n":"欧美","v":"欧美"},{"n":"韩国","v":"韩国"},{"n":"日本","v":"日本"},{"n":"泰国","v":"泰国"},{"n":"印度","v":"印度"},{"n":"俄罗斯","v":"俄罗斯"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
-		"2":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"剧情","v":"剧情"},{"n":"喜剧","v":"喜剧"},{"n":"爱情","v":"爱情"},{"n":"动作","v":"动作"},{"n":"悬疑","v":"悬疑"},{"n":"恐怖","v":"恐怖"},{"n":"奇幻","v":"奇幻"},{"n":"惊悚","v":"惊悚"},{"n":"犯罪","v":"犯罪"},{"n":"科幻","v":"科幻"},{"n":"音乐","v":"音乐"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
-		"3":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"剧情","v":"剧情"},{"n":"喜剧","v":"喜剧"},{"n":"爱情","v":"爱情"},{"n":"动作","v":"动作"},{"n":"悬疑","v":"悬疑"},{"n":"恐怖","v":"恐怖"},{"n":"奇幻","v":"奇幻"},{"n":"惊悚","v":"惊悚"},{"n":"犯罪","v":"犯罪"},{"n":"科幻","v":"科幻"},{"n":"音乐","v":"音乐"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
-		"4":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"剧情","v":"剧情"},{"n":"喜剧","v":"喜剧"},{"n":"爱情","v":"爱情"},{"n":"动作","v":"动作"},{"n":"悬疑","v":"悬疑"},{"n":"恐怖","v":"恐怖"},{"n":"奇幻","v":"奇幻"},{"n":"惊悚","v":"惊悚"},{"n":"犯罪","v":"犯罪"},{"n":"科幻","v":"科幻"},{"n":"音乐","v":"音乐"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
-		"5":[{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
-		"6":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"情感","v":"情感"},{"n":"科幻","v":"科幻"},{"n":"热血","v":"热血"},{"n":"推理","v":"推理"},{"n":"搞笑","v":"搞笑"},{"n":"冒险","v":"冒险"},{"n":"萝莉","v":"萝莉"},{"n":"校园","v":"校园"},{"n":"动作","v":"动作"},{"n":"机战","v":"机战"},{"n":"运动","v":"运动"},{"n":"战争","v":"战争"},{"n":"少年","v":"少年"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"国产","v":"国产"},{"n":"日本","v":"日本"},{"n":"欧美","v":"欧美"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}]
-	},
-	tab_exclude:'夸克网盘|迅雷云盘|百度网盘',
-	// 搜索:muban.首图2.搜索1,
-	搜索: 'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
-	图片来源:'@Referer=https://api.douban.com/@User-Agent=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/113.0.0.0%20Safari/537.36',
-
-	/*
-	lazy代码:源于海阔香雅情大佬 / 小程序：香情影视 https://pastebin.com/L4tHdvFn
-	*/
-	lazy:`js:
+    title: '在线之家',
+    模板: '首图2',
+    // host:'https://www.zxzj.site',
+    host: 'https://www.zxzja.com',
+    url: '/vodshow/fyclassfyfilter.html',
+    filterable: 1,//是否启用分类筛选,
+    filter_url: '-{{fl.area}}-{{fl.by}}-{{fl.class}}-----fypage---{{fl.year}}',
+    filter: 'H4sIAAAAAAAAA+2Y32vaUBTH/5c8+2BitV3/ldGHrARW1nWg3UCK0FbtrHQ2LUXrdHOFVe1QTGkpM5L2n0luzH+xxHvPuRe2hTB8KOy+5fM995x77o9zAndPUZX1l3vKG6OorCub23qhoKSUHf2tEaJ3PCDlasgf9O33xmLcTiRXh0F5GMkhKKUUU5vdcDxTGYDNr92yQBzARg5Mst9kNgYYsz50nS7EpIAxB2fedAYxKaAfJs4B56tduvYxzEcBbPNx3zv5wWwMcL76xHfAxkDI07+Y8TwjQNv1R54nA7QdnQftG7BRKG1EVnoeet7QhePoWt6JnfQ4vg+C9hGEpgC2oN8mPyfMxgD9Ti0yfQQ/Crh1o4H/2ICto4Axezdex4GYFNCvdU26I/CjgLY7i/sxwFw+WZ7dh1wogM19qvhOizRhGZzRu/rgzuBiMRA3t2joeWFzp/fuzEm4uVpaW2Ha4lPQM1zPiLrGdU3UVa6rop7melrQ1Reoh5+Cvsb1NVFf5fqqqOe4nhP1LNezos7Xq4rrVfl6VXG9Kl+vKq5X5esNP8XjeFXkh0Ea5559+tthkNZD0LpnAXa3wqF4G2ybWBfM8nprt8BLelLxalAHhc13eSOadSOlaMvqezG95l97YlzfI4cjv3kGBUMhUS+N6UOkXCeHn8GPQpK+F9eDg96dOzWxISxAlqYszQSlmZGlKUtTluZzLM0VWZqyNGVpPsfSzAqlKa/Cf30Vckvq0uFIUulhh1tAki7ml8fzq32wUcCYjaFvQtIM0GZ+9UfYpSlg9/vTWwlshPll3oA3HQYY89uV1xlDTAqJ/hhdm9QuwY8CzvdkhqNhPgroF/O+5FnhNsEhM1jWe0/HcW38aVLAlOLeX2LedOQPR3aZv3aZ0i+/mY/pOxYAAA==',
+    // tab_exclude: '夸克网盘|迅雷云盘|百度网盘',
+    tab_remove: ['夸克网盘', '迅雷云盘', '百度网盘'],
+    // 搜索:muban.首图2.搜索1,
+    搜索: 'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+    headers: {
+        'User-Agent': 'MOBILE_UA',
+        // 'Referer': HOST,
+    },
+    lazy: `js:
 		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
 		var url = html.url;
 		var from = html.from;
@@ -36,25 +27,29 @@ var rule = {
 		}
 		if (/m3u8|mp4/.test(url)) {
 			input = url
-		} else if (/line3|line5/.test(from)) {
+		} else if (/line3|line4|line5/.test(from)) {
 			var ifrwy = request(url, {
-				headers: {
-					"User-Agent": MOBILE_UA,
-					"Referer": HOST
-				}
+                headers: {
+					'User-Agent': MOBILE_UA,
+					'Referer': HOST,
+					'sec-fetch-mode': 'navigate',
+					'sec-fetch-site': 'cross-site',
+					'sec-fetch-dest': 'iframe',
+					'upgrade-insecure-requests': 1,
+                }
 			});
-			// let code = ifrwy.match(/var url = '(.*?)'/)[1].split('').reverse().join('');
-			let code = '';
-			if (/Cloud/.test(url)) {
-				code = ifrwy.match(/var url = '(.*?)'/)[1].split('').reverse().join('');
-			} else if (/player-v2/.test(url)) {
-				code = ifrwy.match(/data":"(.*?)"/)[1].split('').reverse().join('');
-			}
+			let resultv2 = ifrwy.match(/var result_v2 = {(.*?)};/)[1];
+			let data = JSON.parse('{' + resultv2 + '}').data;
+			let code = data.split('').reverse();
 			let temp = '';
 			for (let i = 0x0; i < code.length; i = i + 0x2) {
 				temp += String.fromCharCode(parseInt(code[i] + code[i + 0x1], 0x10))
 			}
-			input=temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7);
+			input = {
+				jx: 0,
+				url: temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7),
+				parse: 0
+			}
 		} else{
 			input
 		}
