86b4fdd5479a35322267ef052f39dc8292d4315a
潇洒 <78096245+PizazzGY@users.noreply.github.com>
2024-10-13 16:15:03 +0800

    Add files via upload

--- JS/芒果视频.js
+++ JS/芒果视频.js
@@ -135,6 +135,40 @@ var rule = {
         setResult(d);
     }),
 
+    // 搜索: $js.toString(() => {
+    //     fetch_params.headers.Referer = "https://www.mgtv.com";
+    //     fetch_params.headers["User-Agent"] = UA;
+    //     let d = [];
+    //     let html = request(input);
+    //     let json = JSON.parse(html);
+    //     json.data.contents.forEach(function (data) {
+    //         if (data.data.sourceList || data.data.yearList) {
+    //             let list = data.data.sourceList ? data.data.sourceList : data.data.yearList[0].sourceList;
+    //             let desc = "";
+    //             list.forEach(function (it) {
+    //                 desc += it.name + "\t"
+    //             });
+    //             let fyclass = '';
+    //             if (list[0].source === "imgo") {
+    //                 let img = data.data.pic ? data.data.pic : data.data.yearList[0].pic;
+    //                 try {
+    //                     fyclass = data.data.desc.find(it => it.label === '类型').url.match(/lib\/(\d+)/)[1] + '$';
+    //                 } catch (e) {
+    //                     fyclass = '';
+    //                 }
+    //                 log(fyclass);
+    //                 d.push({
+    //                     title: data.data.title ? data.data.title : data.data.yearList[0].title,
+    //                     img: img,
+    //                     content: data.data.story ? data.data.story : data.data.yearList[0].story,
+    //                     desc: data.data.playTime,
+    //                     url: fyclass + list[0].vid
+    //                 })
+    //             }
+    //         }
+    //     });
+    //     setResult(d);
+    // }),
     搜索: $js.toString(() => {
         fetch_params.headers.Referer = "https://www.mgtv.com";
         fetch_params.headers["User-Agent"] = UA;
