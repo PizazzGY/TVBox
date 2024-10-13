87914e61c452beccda9313df000bf615928964b9
潇洒 <78096245+PizazzGY@users.noreply.github.com>
2024-10-13 21:25:21 +0800

    Add files via upload

--- JS/动漫巴士.js
+++ JS/动漫巴士.js
@@ -1,6 +1,6 @@
 var rule = {
     author: '小可乐/240526/第一版',
-    title: '动漫巴士',
+    title: '动漫巴士[漫]',
     host: 'http://dm84.site',
     hostJs: 'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});let src= jsp.pdfh(html,"ul&&a:eq(0)&&href");print(src);HOST=src',
     headers: {'User-Agent': 'MOBILE_UA'},
